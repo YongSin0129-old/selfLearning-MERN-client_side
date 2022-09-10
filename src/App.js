63029ec9aa173eea76afbd4c98130738ef4c42dd
yongsin0129 <yongsin0129@gmail.com>
2022-09-10 13:47:35 +0800

    feat : add nav & home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import NavComponent from './components/nav_component'
+import HomeComponent from './components/home_component'
 
 const App = () => {
-  return <div>react app</div>
+  return (
+    <div>
+      <NavComponent />
+      <Routes>
+        <Route index element={<HomeComponent />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
